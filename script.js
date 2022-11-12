51be3bf58bd0dfda988300441739a849d050b4d4
Tom Jackson <tomcat468@gmail.com>
2022-11-12 14:54:31 +0000

    Make segSet.addEventListener fully functional
    also, remove black border on columns and rows.
    the code is disgusting but it took me days to get it
    working and I wont rewrite it even at gunpoint.

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 let grid = document.querySelector(".container")
-for (let i = 0; i < 16; i++) {
-    let gridColumn = document.createElement("div")
-    gridColumn.className = "column"
-    grid.appendChild(gridColumn)
+let num = 16
+addColumns(num)
+function addColumns (num) {
+    for (let i = 0; i < num; i++) {
+        let gridColumn = document.createElement("div")
+        gridColumn.className = "column"
+        grid.appendChild(gridColumn)
+    }
 }
 
 function addRows(gridColumn) {
-    for (let i = 0; i < 16; i++) {
+   
+    for (let i = 0; i < num; i++) {
         let gridRow = document.createElement("div")
         gridRow.className = "row"
         gridColumn.appendChild(gridRow)
@@ -22,8 +27,41 @@ function pen (gridRow) {
     gridRow.addEventListener('mouseover', () => gridRow.classList.add("black"))
 }
 
-function hover (gridRow) {
-    gridRow.classList.add("black")
-}
-
 rowSegments.forEach(pen)
+
+let segSet = document.querySelector(".segmentChange")
+segSet.addEventListener('click', () => {
+   let answer = prompt("Please enter how many squares per grid side you would like (Maximum: 100):")
+   if (answer < 101) {
+    let grid = document.querySelector(".container")
+    while (grid.firstChild) {
+        grid.removeChild (grid.firstChild)
+    }
+    let num = answer
+    addColumns(num)
+
+    function addRows(gridColumn) {
+   
+        for (let i = 0; i < num; i++) {
+            let gridRow = document.createElement("div")
+            gridRow.className = "row"
+            gridColumn.appendChild(gridRow)
+        }
+    }
+
+    let columns = document.querySelectorAll(".column")
+    columns.forEach(addRows)
+
+    let rowSegments = document.querySelectorAll(".row")
+
+    function pen (gridRow) {
+        gridRow.addEventListener('mouseover', () => gridRow.classList.add("black"))
+    }
+
+    rowSegments.forEach(pen)
+
+   } else {
+    alert ("Number too big!")
+   }
+
+})
